a0e9a1f922a42bc7e6532fd8623550f8e3fb926b
Havyner Caetano <hrcgeo@gmail.com>
2023-05-30 08:20:44 -0300

    fix: change setter types

--- src/components/CreditCardForm.tsx
+++ src/components/CreditCardForm.tsx
@@ -1,6 +1,6 @@
 import { Dispatch, SetStateAction } from "react";
 import { ActionIcon, Flex, Text } from "@mantine/core";
-import { DatePickerInput } from "@mantine/dates";
+import { DatePickerInput, DateValue } from "@mantine/dates";
 import { IconQuestionMark } from "@tabler/icons-react";
 import TextInputCustom from "./TextInputCustom";
 
@@ -11,7 +11,7 @@ type CreditCardFormType = {
     formik: any;
     handleTouching: () => void;
     handleTyping: () => void;
-    setExpirationDate: Dispatch<SetStateAction<Date | null>>;
+    setExpirationDate: (value: DateValue) => void;
     setShouldShowCardBack: Dispatch<SetStateAction<boolean>>;
   };
 };
