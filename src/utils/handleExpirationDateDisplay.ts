4d4d272b3d2a5b62041bdd6cbfdac626bbca2e3d
Havyner Caetano <hrcgeo@gmail.com>
2023-05-30 08:48:47 -0300

    feat: show only the last two digits of the year

--- src/utils/handleExpirationDateDisplay.ts
+++ src/utils/handleExpirationDateDisplay.ts
@@ -6,6 +6,10 @@ export function handleExpirationDateDisplay(expirationDate: Date | null) {
   const monthAsNumber = expirationDate.getMonth() + 1;
   const monthAsString =
     monthAsNumber < 10 ? `0${monthAsNumber}` : monthAsNumber;
+  const lastTwoDigitsOfYear = expirationDate
+    .getFullYear()
+    .toString()
+    .substring(2);
 
-  return `${monthAsString}/${expirationDate.getFullYear()}`;
+  return `${monthAsString}/${lastTwoDigitsOfYear}`;
 }
