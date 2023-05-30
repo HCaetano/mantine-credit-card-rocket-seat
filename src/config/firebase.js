2bfda97aad571a14a93ebf82a28b188ce061f539
Havyner Caetano <hrcgeo@gmail.com>
2023-05-30 08:21:16 -0300

    feat: add get from database do firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAqZO4Yawwb8yXbHuZHSfR0yuKkCKMoYD4",
@@ -10,3 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
