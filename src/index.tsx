7714b97a99ac14fddb81f649e48e182a8d389f72
Havyner Caetano <hrcgeo@gmail.com>
2023-09-06 08:08:17 -0300

    feat: extract app content from App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import Routes from "./routes/Routes";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <StrictMode>
-    <Routes />
+    <App />
   </StrictMode>
 );
 
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,13 +1,13 @@
 import type { ReactElement } from "react";
 import { Route, BrowserRouter, Routes as RouterRoutes } from "react-router-dom";
-import App from "../App";
 import { CreditCardDetails, CreditCardList, Error } from "../pages";
+import Home from "../pages/Home/Home";
 
 const Routes = (): ReactElement => {
   return (
     <BrowserRouter>
       <RouterRoutes>
-        <Route element={<App />} path="/" />
+        <Route element={<Home />} path="/" />
         <Route element={<CreditCardList />} path="/cards" />
         <Route element={<CreditCardDetails />} path="/card/:id" />
         <Route path="*" element={<Error />} />
